0896b055c037580cb86e57116d9443286aa75ae8
mahldcat <mahldcat@gmail.com>
2024-12-03 16:22:53 -0700

    main is working taking in the bearer token and getting data

--- go.mod
+++ go.mod
@@ -1,4 +1,5 @@
-module adventcommandmaingolang
+module github.com/mahldcat/adventcommandmaingolang2024
 
 go 1.18
 
+require github.com/mahldcat/adventlibgolang2024 v0.0.0-20241203230436-0e40a966ee92 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
-	"adventlib/inputfetcher",
+	"errors"
 	"fmt"
+	"os"
+
+	"github.com/mahldcat/adventlibgolang2024/inputfetcher"
 )
+
+func getBearerToken() (string, error) {
+	// os.Args[0] is the program name, and subsequent elements are the arguments
+	if len(os.Args) < 2 {
+		return "", errors.New("Need to pass the bearer token in as arg 1")
+	}
+	return os.Args[1], nil
+}
 
 func main() {
 
-	bearerToken := "your-bearer-token-here"
+	bearerToken, err := getBearerToken()
+	if err != nil {
+		fmt.Printf("Error getting bearer token: %w", err)
+		os.Exit(-1)
+	}
+
 	fetcher := inputfetcher.NewDataFetcher(bearerToken)
 
 	data, err := fetcher.FetchDay1Data()
