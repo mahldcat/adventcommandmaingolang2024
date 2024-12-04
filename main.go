18a3d0ce1dd049bfb7859d024d6a7749469643ed
mahldcat <mahldcat@gmail.com>
2024-12-03 22:02:02 -0700

    restructuring things so I can add new challenge handlers

--- main.go
+++ main.go
@@ -1,53 +1,61 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
+	"strconv"
 
-	"github.com/mahldcat/adventlibgolang2024/day1"
 	"github.com/mahldcat/adventlibgolang2024/inputfetcher"
 )
 
-func getBearerToken() (string, error) {
+func getBearerToken() string {
+	// os.Args[0] is the program name, and subsequent elements are the arguments
+	if len(os.Args) < 3 {
+		fmt.Println("pass the bearer token in as the second arg on the command line")
+		os.Exit(-1)
+	}
+	return os.Args[2]
+}
+
+func getHandlerId() int {
 	// os.Args[0] is the program name, and subsequent elements are the arguments
 	if len(os.Args) < 2 {
-		return "", errors.New("Need to pass the bearer token in as arg 1")
+		fmt.Println("pass the bearer token in as the second arg on the command line")
+		os.Exit(-1)
 	}
-	return os.Args[1], nil
+
+	hId, err := strconv.Atoi(os.Args[1])
+
+	if err != nil {
+		fmt.Printf("Error parsing '%s'\n", os.Args[1])
+		os.Exit(-1)
+	}
+
+	return hId
+}
+
+func getDataFetcher() inputfetcher.DataFetcher {
+	bearerToken := getBearerToken()
+
+	return inputfetcher.NewDataFetcher(bearerToken)
+}
+
+var dailyHandlers = map[int]func(inputfetcher.DataFetcher){
+	1: HandleDay1Solution, //in day1solnhandler
 }
 
 func main() {
 
-	bearerToken, err := getBearerToken()
-	if err != nil {
-		fmt.Println("Error getting bearer token", err)
-		os.Exit(-1)
+	dataFetcher := getDataFetcher()
+
+	handlerId := getHandlerId()
+
+	handler := dailyHandlers[handlerId]
+	if handler == nil {
+		fmt.Printf("Handler for Id %d not found", handlerId)
+		os.Exit(-99)
 	}
 
-	fetcher := inputfetcher.NewDataFetcher(bearerToken)
-
-	left, right, err := fetcher.FetchDay1Data()
-
-	if err != nil {
-		fmt.Println("Error on Data Fetch", err)
-		os.Exit(-2)
-	}
-
-	sln, err := day1.SolveDay1Part1(left, right)
-	if err != nil {
-		fmt.Println("Error on Day1.1 solution", err)
-		os.Exit(-3)
-	}
-
-	fmt.Printf("Day 1.1 Solution: %d\n", sln)
-
-	sln, err = day1.SolveDay1Part2(left, right)
-	if err != nil {
-		fmt.Println("Error on Day1.2 solution", err)
-		os.Exit(-4)
-	}
-
-	fmt.Printf("Day 1.2 Solution: %d\n", sln)
+	handler(dataFetcher)
 
 }
