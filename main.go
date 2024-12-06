afcf69f68bdfc82483efc7f89c4f49d8a722c82e
mahldcat <mahldcat@gmail.com>
2024-12-05 22:01:13 -0700

    getting the main app working again

--- main.go
+++ main.go
@@ -44,7 +44,27 @@ func getDataFetcher() inputfetcher.DataFetcher {
 var dailyHandlers = map[int]func(inputfetcher.DataFetcher){
 	1: dayhandlers.HandleDay1Solution, //in day1solnhandler
 	4: dayhandlers.HandleDay4Solution,
-	5: dayhandlers.HandleDay5olution
+	5: dayhandlers.HandleDay5Solution,
+	6: dayhandlers.HandleDay6Solution,
+	7: dayhandlers.HandleDay7Solution,
+
+	8:  dayhandlers.HandleDay8Solution,
+	9:  dayhandlers.HandleDay9Solution,
+	10: dayhandlers.HandleDay10Solution,
+	11: dayhandlers.HandleDay11Solution,
+	12: dayhandlers.HandleDay12Solution,
+	13: dayhandlers.HandleDay13Solution,
+	14: dayhandlers.HandleDay14Solution,
+	15: dayhandlers.HandleDay15Solution,
+	16: dayhandlers.HandleDay16Solution,
+	17: dayhandlers.HandleDay17Solution,
+	18: dayhandlers.HandleDay18Solution,
+	19: dayhandlers.HandleDay19Solution,
+	20: dayhandlers.HandleDay20Solution,
+	21: dayhandlers.HandleDay21Solution,
+	22: dayhandlers.HandleDay22Solution,
+	23: dayhandlers.HandleDay23Solution,
+	24: dayhandlers.HandleDay24Solution,
 }
 
 func main() {
@@ -53,8 +73,8 @@ func main() {
 
 	handlerId := getHandlerId()
 
-	handler := dailyHandlers[handlerId]
-	if handler == nil {
+	handler, exists := dailyHandlers[handlerId]
+	if !exists {
 		fmt.Printf("Handler for Id %d not found", handlerId)
 		os.Exit(-99)
 	}
